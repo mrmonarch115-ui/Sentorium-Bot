461f2493204a93cc10072744f08bd8b4c262e920
mrmonarch115-ui <mrmonarch115@gmail.com>
2026-04-16 19:37:49 +0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#A9A9A9", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
